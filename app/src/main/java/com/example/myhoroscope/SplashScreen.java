20e1c85a54d6c6cd4c387076fd2f213857f91df1
Cristina Ailoaei <c4.criss@yahoo.com>
2019-04-26 16:37:31 +0200

    Implement correct version of splash screen

--- app/src/main/java/com/example/myhoroscope/SplashScreen.java
+++ app/src/main/java/com/example/myhoroscope/SplashScreen.java
@@ -3,22 +3,19 @@ package com.example.myhoroscope;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.WindowManager;
 
 public class SplashScreen extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        getSupportActionBar().hide();
-        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
-        setContentView(R.layout.activity_splash_screen);
+
 
         Thread myThread = new Thread() {
             @Override
             public void run() {
                 try {
-                    sleep(3000);
+                    sleep(500);
                     Intent intent = new Intent(getApplicationContext(), MainActivity.class);
                     startActivity(intent);
                     finish();
