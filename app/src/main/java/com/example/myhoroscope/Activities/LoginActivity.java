fc15f5e8a87f10172a4707eeb63a88b05e02f634
Cristina Ailoaei <c4.criss@yahoo.com>
2019-04-27 22:25:56 +0200

    Complete user login using firebase

---
+++ app/src/main/java/com/example/myhoroscope/Activities/LoginActivity.java
@@ -0,0 +1,129 @@
+package com.example.myhoroscope.Activities;
+
+import android.content.Intent;
+import android.support.annotation.NonNull;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.ProgressBar;
+import android.widget.Toast;
+
+import com.example.myhoroscope.R;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+public class LoginActivity extends AppCompatActivity {
+
+    private EditText userMail, userPassword;
+    private Button loginBtn;
+    private ProgressBar loginProgress;
+    private Intent homeActivity;
+    private FirebaseAuth auth;
+    private ImageView imgUserPhoto;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        userMail = findViewById(R.id.loginMail);
+        userPassword = findViewById(R.id.loginPassword);
+        loginBtn = findViewById(R.id.loginButton);
+        loginProgress = findViewById(R.id.loginProgressBar);
+        auth = FirebaseAuth.getInstance();
+        homeActivity = new Intent(this, HomeActivity.class);
+        imgUserPhoto = findViewById(R.id.loginUserPhoto);
+
+        imgUserPhoto.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Intent registerActivity = new Intent(getApplicationContext(), RegisterActivity.class);
+                startActivity(registerActivity);
+                finish();
+
+            }
+        });
+
+        loginProgress.setVisibility(View.INVISIBLE);
+
+        loginBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                loginBtn.setVisibility(View.INVISIBLE);
+                loginProgress.setVisibility(View.VISIBLE);
+
+                final String mail = userMail.getText().toString();
+                final String password = userPassword.getText().toString();
+
+                if(mail.isEmpty() || password.isEmpty()) {
+
+                    showMessage("Please verify the fields");
+                    loginBtn.setVisibility(View.VISIBLE);
+                    loginProgress.setVisibility(View.INVISIBLE);
+                }
+                else {
+                    signIn(mail, password);
+                }
+                
+            }
+        });
+
+    }
+
+    private void signIn(String mail, String password) {
+
+        auth.signInWithEmailAndPassword(mail, password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+
+                if(task.isSuccessful()) {
+
+                    loginProgress.setVisibility(View.INVISIBLE);
+                    loginBtn.setVisibility(View.VISIBLE);
+                    updateUI();
+                }
+                else {
+
+                    loginBtn.setVisibility(View.VISIBLE);
+                    loginProgress.setVisibility(View.INVISIBLE);
+                    showMessage(task.getException().getMessage());
+                }
+
+            }
+        });
+
+
+    }
+
+    private void updateUI() {
+
+        startActivity(homeActivity);
+        finish();
+    }
+
+    private void showMessage(String message) {
+
+        Toast.makeText(getApplicationContext(), message, Toast.LENGTH_LONG).show();
+    }
+/*
+    @Override
+    protected void onStart() {
+
+        super.onStart();
+        FirebaseUser user = auth.getCurrentUser();
+
+        if(user != null) {
+
+            updateUI();
+        }
+    }
+    */
+}
--- app/src/main/java/com/example/myhoroscope/Activities/MainActivity.java
+++ app/src/main/java/com/example/myhoroscope/Activities/MainActivity.java
@@ -10,15 +10,17 @@ import com.example.myhoroscope.R;
 
 public class MainActivity extends AppCompatActivity {
 
-    Button button;
+    Button button1, button2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        button = findViewById(R.id.registeractivity);
-        button.setOnClickListener(new View.OnClickListener()
+        button1 = findViewById(R.id.registeractivity);
+        button2 = findViewById(R.id.loginactivity);
+
+        button1.setOnClickListener(new View.OnClickListener()
         {
             @Override
             public void onClick(View v)
@@ -26,11 +28,24 @@ public class MainActivity extends AppCompatActivity {
                 openRegisterActivity();
             }
         });
+
+        button2.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View v)
+            {
+                openLoginActivity();
+            }
+        });
+    }
+
+    private void openLoginActivity() {
+        Intent intent = new Intent(this, LoginActivity.class);
+        startActivity(intent);
     }
 
     public void openRegisterActivity() {
         Intent intent = new Intent(this, RegisterActivity.class);
         startActivity(intent);
-
     }
 }
