96e0d53d9e0dadf29632c3b2f030f8d1b84651cc
Cristina Ailoaei <c4.criss@yahoo.com>
2019-04-27 20:22:17 +0200

    Complete user registration using firebase

--- app/src/main/java/com/example/myhoroscope/Activities/RegisterActivity.java
+++ app/src/main/java/com/example/myhoroscope/Activities/RegisterActivity.java
@@ -5,16 +5,30 @@ import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.net.Uri;
 import android.os.Build;
+import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.ProgressBar;
 import android.widget.Toast;
 
 import com.example.myhoroscope.R;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.auth.UserProfileChangeRequest;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
 
 public class RegisterActivity extends AppCompatActivity {
 
@@ -23,16 +37,59 @@ public class RegisterActivity extends AppCompatActivity {
     static int REQUESTCODE = 1;
     Uri pickedImgUri;
 
+    private EditText userName, userEmail, userPassword, userConfirmPassword;
+    private ProgressBar loadingProgress;
+    private Button registerButton;
+
+    private FirebaseAuth auth;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
 
+        userName = findViewById(R.id.registerName);
+        userEmail = findViewById(R.id.registerEmail);
+        userPassword = findViewById(R.id.registerPassword);
+        userConfirmPassword = findViewById(R.id.registerConfirmPassword);
+        loadingProgress = findViewById(R.id.registerProgressBar);
+        registerButton = findViewById(R.id.registerBtn);
+
+        loadingProgress.setVisibility(View.INVISIBLE);
+
+        auth = FirebaseAuth.getInstance();
+
+        registerButton.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View view)
+            {
+                registerButton.setVisibility(View.INVISIBLE);
+                loadingProgress.setVisibility(View.VISIBLE);
+
+                final String name = userName.getText().toString();
+                final String email = userEmail.getText().toString();
+                final String password = userPassword.getText().toString();
+                final String confirmPassword = userConfirmPassword.getText().toString();
+
+                if(email.isEmpty() || name.isEmpty() || password.isEmpty() || confirmPassword.isEmpty() || !password.equals(confirmPassword)) {
+
+                    showMessage("Please check if all the fields have been filled out correctly");
+                    registerButton.setVisibility(View.VISIBLE);
+                    loadingProgress.setVisibility(View.INVISIBLE);
+                }
+                else {
+                    createUserAccount(email, name, password);
+                }
+            }
+        });
+
         imgUserPhoto = findViewById(R.id.registerUserPhoto);
         imgUserPhoto.setOnClickListener(new View.OnClickListener() {
+
             @Override
             public void onClick(View v) {
-                if(Build.VERSION.SDK_INT >= 22)
+                if(Build.VERSION.SDK_INT >= 23)
                     checkAndRequestForPermission();
                 else
                     openGallery();
@@ -42,8 +99,70 @@ public class RegisterActivity extends AppCompatActivity {
 
     }
 
+    private void createUserAccount(String email, final String name, String password) {
+
+        auth.createUserWithEmailAndPassword(email, password).addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if(task.isSuccessful()) {
+                    showMessage("The account has been created");
+                    updateUserInfo(name, pickedImgUri, auth.getCurrentUser());
+                }
+            }
+        });
+    }
+
+    private void updateUserInfo(final String name, Uri pickedImgUri, final FirebaseUser currentUser) {
+
+        StorageReference storage = FirebaseStorage.getInstance().getReference().child("users_photos");
+        final StorageReference imageFilePath = storage.child(pickedImgUri.getLastPathSegment());
+
+        imageFilePath.putFile(pickedImgUri).addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+
+            @Override
+            public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+
+                imageFilePath.getDownloadUrl().addOnSuccessListener(new OnSuccessListener<Uri>() {
+
+                    @Override
+                    public void onSuccess(Uri uri) {
+
+                        UserProfileChangeRequest profileUpdate = new UserProfileChangeRequest.Builder()
+                                .setDisplayName(name).setPhotoUri(uri).build();
+
+                        currentUser.updateProfile(profileUpdate).addOnCompleteListener(new OnCompleteListener<Void>() {
+                            @Override
+                            public void onComplete(@NonNull Task<Void> task) {
+
+                                if(task.isSuccessful()) {
+
+                                    showMessage("Register complete");
+                                    updateUI();
+                                }
+                            }
+                        });
+                    }
+                });
+            }
+        });
+    }
+
+    private void updateUI() {
+
+        Intent intent = new Intent(getApplicationContext(), HomeActivity.class);
+        startActivity(intent);
+        finish();
+    }
+
+    private void showMessage(String message) {
+
+        Toast.makeText(getApplicationContext(), message, Toast.LENGTH_LONG).show();
+    }
+
     @Override
     protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+
         super.onActivityResult(requestCode, resultCode, data);
 
         if(resultCode == RESULT_OK && requestCode == REQUESTCODE && data != null) {
