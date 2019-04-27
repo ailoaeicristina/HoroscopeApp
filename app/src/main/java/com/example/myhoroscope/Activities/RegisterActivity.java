5380a1cba2d365910cb087374bd2424b8b475a59
Cristina Ailoaei <c4.criss@yahoo.com>
2019-04-27 18:05:57 +0200

    Permission request for gallery access

--- app/src/main/java/com/example/myhoroscope/Activities/RegisterActivity.java
+++ app/src/main/java/com/example/myhoroscope/Activities/RegisterActivity.java
@@ -1,15 +1,79 @@
 package com.example.myhoroscope.Activities;
 
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.net.Uri;
+import android.os.Build;
+import android.support.annotation.Nullable;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.Toast;
 
 import com.example.myhoroscope.R;
 
 public class RegisterActivity extends AppCompatActivity {
 
+    ImageView imgUserPhoto;
+    static int PReqCode = 1;
+    static int REQUESTCODE = 1;
+    Uri pickedImgUri;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+
+        imgUserPhoto = findViewById(R.id.registerUserPhoto);
+        imgUserPhoto.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(Build.VERSION.SDK_INT >= 22)
+                    checkAndRequestForPermission();
+                else
+                    openGallery();
+            }
+
+        });
+
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if(resultCode == RESULT_OK && requestCode == REQUESTCODE && data != null) {
+            pickedImgUri = data.getData();
+            imgUserPhoto.setImageURI(pickedImgUri);
+        }
+    }
+
+    private void checkAndRequestForPermission() {
+        if(ContextCompat.checkSelfPermission(RegisterActivity.this, Manifest.permission.READ_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
+            if(ActivityCompat.shouldShowRequestPermissionRationale(RegisterActivity.this, Manifest.permission.READ_EXTERNAL_STORAGE)) {
+                Toast.makeText(RegisterActivity.this, "Accept permission in order to upload a picture", Toast.LENGTH_SHORT).show();
+            }
+            else
+            {
+                ActivityCompat.requestPermissions(RegisterActivity.this,
+                        new String[]{ Manifest.permission.READ_EXTERNAL_STORAGE}, PReqCode);
+            }
+        }
+        else
+        {
+            openGallery();
+        }
+
+    }
+
+    private void openGallery() {
+
+        Intent galleryIntent = new Intent(Intent.ACTION_GET_CONTENT);
+        galleryIntent.setType("image/");
+        startActivityForResult(galleryIntent, REQUESTCODE);
     }
 }
