e0ba31acb37f8651137d57edf727c62b2583d57f
Cristina Ailoaei <c4.criss@yahoo.com>
2019-04-28 09:43:35 +0200

    Remember login credential using SharedPreferences

--- app/src/main/java/com/example/myhoroscope/Activities/LoginActivity.java
+++ app/src/main/java/com/example/myhoroscope/Activities/LoginActivity.java
@@ -1,17 +1,21 @@
 package com.example.myhoroscope.Activities;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
+import android.preference.PreferenceManager;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.CheckBox;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.ProgressBar;
 import android.widget.Toast;
 
 import com.example.myhoroscope.R;
+import com.google.android.gms.flags.impl.SharedPreferencesFactory;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
@@ -20,8 +24,12 @@ import com.google.firebase.auth.FirebaseUser;
 
 public class LoginActivity extends AppCompatActivity {
 
+    private SharedPreferences preferences;
+    private SharedPreferences.Editor editor;
+
     private EditText userMail, userPassword;
     private Button loginBtn;
+    private CheckBox checkBox;
     private ProgressBar loginProgress;
     private Intent homeActivity;
     private FirebaseAuth auth;
@@ -34,11 +42,16 @@ public class LoginActivity extends AppCompatActivity {
 
         userMail = findViewById(R.id.loginMail);
         userPassword = findViewById(R.id.loginPassword);
+        checkBox = findViewById(R.id.loginCheckBox);
+        preferences = PreferenceManager.getDefaultSharedPreferences(this);
+        editor =  preferences.edit();
         loginBtn = findViewById(R.id.loginButton);
         loginProgress = findViewById(R.id.loginProgressBar);
         auth = FirebaseAuth.getInstance();
         homeActivity = new Intent(this, HomeActivity.class);
         imgUserPhoto = findViewById(R.id.loginUserPhoto);
+
+        checkSharedPreferences();
 
         imgUserPhoto.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -63,6 +76,30 @@ public class LoginActivity extends AppCompatActivity {
                 final String mail = userMail.getText().toString();
                 final String password = userPassword.getText().toString();
 
+                if(checkBox.isChecked()) {
+
+                    editor.putString(getString(R.string.checkbox), "True");
+                    editor.commit();
+
+                    editor.putString(getString(R.string.mail), mail);
+                    editor.commit();
+
+                    editor.putString(getString(R.string.password), password);
+                    editor.commit();
+                }
+                else {
+
+                    editor.putString(getString(R.string.checkbox), "False");
+                    editor.commit();
+
+                    editor.putString(getString(R.string.mail), "");
+                    editor.commit();
+
+                    editor.putString(getString(R.string.password), "");
+                    editor.commit();
+
+                }
+
                 if(mail.isEmpty() || password.isEmpty()) {
 
                     showMessage("Please verify the fields");
@@ -72,10 +109,25 @@ public class LoginActivity extends AppCompatActivity {
                 else {
                     signIn(mail, password);
                 }
-                
+
             }
         });
 
+    }
+
+    private void checkSharedPreferences() {
+
+        String checkbox = preferences.getString(getString(R.string.checkbox), "False");
+        String mail = preferences.getString(getString(R.string.mail), "");
+        String password =  preferences.getString(getString(R.string.password), "");
+
+        userMail.setText(mail);
+        userPassword.setText(password);
+
+        if(checkbox.equals("True"))
+            checkBox.setChecked(true);
+        else
+            checkBox.setChecked(false);
     }
 
     private void signIn(String mail, String password) {
@@ -127,3 +179,4 @@ public class LoginActivity extends AppCompatActivity {
     }
     */
 }
+
